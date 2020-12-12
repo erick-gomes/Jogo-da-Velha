cbdb1ab21e59f8e184878fcc40e8f3034588d905
brlouco <brlouco88@hotmail.com>
2020-12-12 04:34:43 -0300

    Formulário de escolha da dificuldade

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Jogo da Velha</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">       
     <link rel="stylesheet" href="css/estilos.css">
-    <script src="js/events.js"></script>
 </head>
 <body>
     <header>
@@ -14,16 +13,16 @@
     </header>
     <main>
         
-        <form action="game/" method="post">
+        <form action="game/index.php" method="get">
             <div class="form-check">
                 <input class="form-check-input" type="radio" 
-                name="mode" id="exampleRadios1" value="easy" checked>
+                name="mode" id="exampleRadios1" value="0" checked>
                 <label class="form-check-label" for="exampleRadios1">Modo fácil</label>
             </div>
             <!-- ========================================================= -->
             <div class="form-check">
                 <input class="form-check-input" type="radio" 
-                name="mode" id="exampleRadios2" value="hard">
+                name="mode" id="exampleRadios2" value="1">
                 <label class="form-check-label" for="exampleRadios2">Modo difícil</label> 
             </div>
             <!-- ========================================================= -->
